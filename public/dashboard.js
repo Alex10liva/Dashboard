32a0bc6a0b51beadd67164df46c7c3049bd65077
Alex10liva <alex_oliv1000@hotmail.com>
2023-04-21 16:30:24 -0600

    Left sidebar tweaks and main content added

--- public/dashboard.js
+++ public/dashboard.js
@@ -1,4 +1,41 @@
-function buttonSidebar(n){
-    let sidebar = document.querySelector(`.left-sidebar ul li:nth-child(${n}) #svg-div`);
-    sidebar.classList.toggle('active');
+function buttonSidebar(n) {
+    let sidebarSvg = document.querySelector(`.left-sidebar ul li:nth-child(${n}) #svg-div`);
+    sidebarSvg.classList.toggle('activeSvg');
+
+    let sidebarPaths = document.querySelectorAll(`.left-sidebar ul li:nth-child(${n}) #svg-div .arrow-icon path`);
+    sidebarPaths.forEach(path => path.classList.toggle('activeSvg-C'));
+
+    let sidebarDiv = document.querySelector(`.left-sidebar ul li:nth-child(${n})`);
+    sidebarDiv.classList.toggle('activeDiv')
+
+    let sideBarN = document.querySelector(`.left-sidebar ul`);
+
+    for (let i = 1; i < sideBarN.childElementCount + 1; i++) {
+        if (i != n) {
+            sidebarSvg = document.querySelector(`.left-sidebar ul li:nth-child(${i}) #svg-div`);
+            if (sidebarSvg.classList.contains('activeSvg')) {
+                sidebarSvg.classList.remove('activeSvg')
+            }
+
+            sidebarPaths = document.querySelectorAll(`.left-sidebar ul li:nth-child(${i}) #svg-div .arrow-icon path`);
+            for (let i = 0; i < sidebarPaths.length; i++) {
+                if (sidebarPaths[i].classList.contains('activeSvg-C')) {
+                    sidebarPaths[i].classList.remove('activeSvg-C')
+                }
+            }
+            sidebarDiv = document.querySelector(`.left-sidebar ul li:nth-child(${i})`);
+            if (sidebarDiv.classList.contains('activeDiv')) {
+                sidebarDiv.classList.remove('activeDiv')
+            }
+        }
+    }
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    var calendarEl = document.getElementById('calendar-content');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        locale: 'es',
+        initialView: 'dayGridMonth'
+    });
+    calendar.render();
+});
